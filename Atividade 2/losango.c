b78cfa3b8c8dcc7db32a68a61db73ff3088cf45e
Arthurclima <arthur92lima@gmail.com>
2025-03-28 18:33:00 -0300

    losango

--- Atividade 2/losango.c
+++ Atividade 2/losango.c
@@ -3,7 +3,13 @@
 
 int main(int argc, char* argv[]){
 
-
+    float diagonal_maior, diagonal_menor, area;
+    printf("Digite a diagonal maior: ");
+    scanf("%f", &diagonal_maior);
+    printf("Digite a diagonal menor: ");
+    scanf("%f", &diagonal_menor);
+    area = (diagonal_maior*diagonal_menor)/2;
+    printf("A area do losango eh: %.1f\n", area);
 
 
 
