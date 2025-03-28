6d24a3fcb91cb350ccd8d3e1170669fee5c6a116
Arthurclima <arthur92lima@gmail.com>
2025-03-28 11:42:37 -0300

    operadores

---
+++ tipos_de_dados/operadores.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+
+    float n1, n2, resultado_adicao;
+
+    printf("Digite o valor de n1: ");
+    scanf("%f", &n1);
+
+    printf("Digite o valor de n2: ");
+    scanf("%f", &n2);
+
+    resultado_adicao = n1+n2;
+    
+    printf("O resultado eh: %f", resultado_adicao);
+
+
+
+    // Adição
+
+    // Subtração
+
+    // Multiplicção
+
+    // Divisão
+
+    // Potenciação
+
+    // Radiciação
+
+    // Resto da Divisão
+
+    // Quociente da Divisão
+
+    // Incremento
+
+    // Decremento
+
+
+
+
+
+    return 0;
+}
