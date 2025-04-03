4bd7cf5d550b2874195ca3fc9cfa58fcc455851a
Arthurclima <arthur92lima@gmail.com>
2025-04-03 11:44:25 -0300

    imc corrigido

--- Atividade 1/imc.c
+++ Atividade 1/imc.c
@@ -3,12 +3,26 @@
 int main (int argc, char* argv[]){
 
 float imc, peso, altura;
-printf("Digite o peso:");
+printf("Digite o peso: ");
 scanf("%f", &peso);
-printf("Digite a altura:");
+printf("Digite a altura: ");
 scanf("%f", &altura);
 imc = peso /(altura*altura);
 printf("O imc eh: %.2f\n", imc);
 
-
+if (imc <= 18.5)  {
+    printf("Classificacao: Abaixo do peso\n"); 
+}
+else if (imc > 18.5 && imc <= 24.9) {
+    printf("Classificacao: Peso normal\n"); 
+}
+else if (imc > 24.9 && imc <= 29.9) {
+    printf("Classificacao: Sobrepeso\n");
+}
+else if (imc > 29.9 && imc <= 34.9 ) {
+    printf("Classificacao: Obesidade Grau 1\n");
+}
+else if (imc > 34.9 && imc <= 39.9) {
+    printf("Classificacao: Obesidade Grau 2\n");
+}
 }
