ec36dc730df2ea406301c224844b68f556f9af16
Arthurclima <arthur92lima@gmail.com>
2025-04-04 11:58:48 -0300

    atividade 3

---
+++ Atividade 3/filmes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+    char opcao
+    printf("Escolha uma opcao de filme: \n");
+    printf("L - Leve\n")
+    printf("I - Intenso\n")
+
+    scanf ("%c", &opcao);
+    switch (opcao){
+
+        case 'L':
+            printf("Voce escolheu um filme leve!\n");
+            printf("Agora escolha um genero de filme leve: \n");
+            printf("C - Comédia\n");
+            printf("A - Animação\n");
+        
+            
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
