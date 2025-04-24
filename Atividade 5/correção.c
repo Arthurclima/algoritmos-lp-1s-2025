82f333e44ce652c4bc69b72f2bae9dba074ca787
Arthurclima <arthur92lima@gmail.com>
2025-04-24 12:03:46 -0300

    correção

--- Atividade 5/correção.c
+++ Atividade 5/correção.c
@@ -2,8 +2,50 @@
 
 int main(int argc, char* argv[]){
     
-    
-    
+    int qtdAlunos = 0;
+
+    printf("Informe a quantidade de alunos: ");
+    scanf("%d", &qtdAlunos);
+
+    float notas[qtdAlunos];
+
+    for(int i = 0; i < qtdAlunos; i++){
+        do {
+            printf("Digite a nota do aluno %d", i+1);
+            scanf("%f", &notas [i]);
+        } while (notas [i] > 10.0 || notas [i] < 0.0);
+    }
+
+    int posicaoNota
+
+    printf("\nInforme a posicao da nota a ser apresentada: ");
+    scanf("%d", &posicaoNota);
+
+    float somanotas = 0.0;
+    float medianotas = 0.0;
+    float menorNota = notas[0];
+    float maiorNota = notas [0];
+    float qtdAlunosAprovados = 0;
+    for(int i=0; i < qtdAlunos; i++) {
+        somanotas += notas [i];
+
+        if(menorNota > notas[i]){
+            menorNota > notas[i];
+        }
+        if (maiorNota < notas [i]){
+            maiorNota = notas[i]
+        }
+        if(notas[i] >= 6.0){
+            qtdAlunosAprovados++;
+        }
+    }
+
+    medianotas = somanotas / qtdAlunos;
+    printf("\nmedia das notas: %.2f", medianotas);
+    printf("\nmenor nota: %.2f", menorNota);
+    printf("\nmaior nota: %.2f", maiorNota);
+    printf("\nqtd. alunos aprovados: %d", qtdAlunosAprovados);
+    printf("\nnota do aluno %d: %.2f", posicaoNota, notas[posicaoNota]);
     
     
     
