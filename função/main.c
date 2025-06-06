183e83174e7f75b37f787ddbd4210965bb5ec1dc
Arthurclima <arthur92lima@gmail.com>
2025-06-06 10:49:48 -0300

    changes

--- função/main.c
+++ função/main.c
@@ -1,36 +1,37 @@
 #include <stdio.h>
 #include <string.h>
-#include "biblioteca.h"
 #include <stdlib.h>
+#include "biblioteca.h"
 
 int main(int argc, char* argv[]){
-
+    
     if(argc < 3){
         printf("Argumentos invalidos");
         return 1;
     }
 
-    if (strcmp(argv[1], "-maior") == 0){
+    if(strcmp(argv[1], "-maior") == 0){
         if(argc != 4){
             printf("Argumentos invalidos");
             return 1;
-        }
+        }   
 
-        int z = atoi(argv[1]);
         int x = atoi(argv[2]);
-        printf("%d", maior_2n(x,z));
+        int y = atoi(argv[3]);
+        printf("%d", maior_2n(x, y));
 
     }
 
-     if (strcmp(argv[1], "-primo") == 0){
+    if(strcmp(argv[1], "-primo") == 0){
         if(argc != 3){
-            printf ("Argumentos Invalidos");
+            printf("Argumentos invalidos");
             return 1;
-        }
+        }   
 
         int x = atoi(argv[2]);
+        printf("%d", verificar_numero_primo(x));
 
-     }
-    
+    }
+
     return 0;
 }
